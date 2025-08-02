8eddacca133681c21fb80ec1b8d80beeec93d368
Jing Hee <jingheeshee@gmail.com>
2025-08-02 17:50:47 +0800

    Add files via upload

---
+++ shopping_cart.py
@@ -0,0 +1,63 @@
+class Product():
+    def __init__():
+        products = {
+            "Category1": {
+                "001" : {
+                    "name" : "The name",
+                    "price": 0.00,
+                    "stock": 0
+                }
+                "001" : {
+                    "name" : "The name",
+                    "price": 0.00,
+                    "stock": 0
+                }
+                "001" : {
+                    "name" : "The name",
+                    "price": 0.00,
+                    "stock": 0
+                }
+            }
+            "Category1": {
+                "001" : {
+                    "name" : "The name",
+                    "price": 0.00,
+                    "stock": 0
+                }
+                "001" : {
+                    "name" : "The name",
+                    "price": 0.00,
+                    "stock": 0
+                }
+                "001" : {
+                    "name" : "The name",
+                    "price": 0.00,
+                    "stock": 0
+                }
+            }
+            "Category1": {
+                "001" : {
+                    "name" : "The name",
+                    "price": 0.00,
+                    "stock": 0
+                }
+                "001" : {
+                    "name" : "The name",
+                    "price": 0.00,
+                    "stock": 0
+                }
+                "001" : {
+                    "name" : "The name",
+                    "price": 0.00,
+                    "stock": 0
+                }
+            }
+        }
+    def display_product():
+        print("something")
+
+
+
+
+class ShoppingCart():
+    def display_all():
